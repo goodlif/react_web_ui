55c480c35ee38a6584b874b5210d3e9551270575
Jonathan George <jonogeorge@gmail.com>
2019-02-10 18:20:16 +0200

    Updates

---
+++ precache-manifest.471e3a9de7f2c6b7a2fd17e7460e6581.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d59d2f163dd6b4d3cf4b",
+    "url": "/react-web-ui/static/js/runtime~main.d59d2f16.js"
+  },
+  {
+    "revision": "20f60f70f38bca641f45",
+    "url": "/react-web-ui/static/js/main.20f60f70.chunk.js"
+  },
+  {
+    "revision": "a224c32d0d4bc13f8988",
+    "url": "/react-web-ui/static/js/1.a224c32d.chunk.js"
+  },
+  {
+    "revision": "2f8899470bc2e6d6f661d37e26c65c3b",
+    "url": "/react-web-ui/index.html"
+  }
+];
